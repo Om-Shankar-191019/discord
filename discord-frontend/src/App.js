22b2cb0331b14839ff115869ed875a100ac6c481
om shankar <om8003golu@gmail.com>
2024-01-23 16:31:47 +0530

    set up routes at frontend

--- discord-frontend/src/App.js
+++ discord-frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./Dashboard/Dashboard";
+import LoginPage from "./authPages/LoginPage/LoginPage";
+import RegisterPage from "./authPages/RegisterPage/RegisterPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
