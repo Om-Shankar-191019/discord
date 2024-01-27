e75f5c62cfed40780576d71ed23c0b3a83204b59
om shankar <om8003golu@gmail.com>
2024-01-27 20:54:55 +0530

    logout functionality and starting socketio

---
+++ discord-frontend/src/Dashboard/AppBar/DropdownMenu.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutReducer } from "../../store/reducers/authSlice";
+
+export default function BasicMenu() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(logoutReducer());
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <IconButton onClick={handleMenuOpen} style={{ color: "white" }}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
